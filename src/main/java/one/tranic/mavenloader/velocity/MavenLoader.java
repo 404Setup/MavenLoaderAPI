612158fe2c6f6721463bef7c56bea952d1760555
404Setup <153366651+404Setup@users.noreply.github.com>
2024-10-14 15:25:33 +0800

    Do not save the update record in Velocity, because it will not be used

--- src/main/java/one/tranic/mavenloader/velocity/MavenLoader.java
+++ src/main/java/one/tranic/mavenloader/velocity/MavenLoader.java
@@ -23,25 +23,20 @@ import java.util.Objects;
 
 @Plugin(id = "maven-loader", name = "MavenLoader", version = BuildConstants.VERSION, url = "https://tranic.one", authors = {"404"})
 public class MavenLoader {
+    private static Updater updater;
     private final Logger logger;
     private final Metrics.Factory metricsFactory;
     private final @DataDirectory Path dataDirectory;
     private final ProxyServer proxy;
     private Metrics metrics;
-    private static Updater updater;
-    private static UpdateRecord updateRecord;
 
     @Inject
-    public MavenLoader(Logger logger, ProxyServer proxy , Metrics.Factory metricsFactory, @DataDirectory Path dataDirectory) {
+    public MavenLoader(Logger logger, ProxyServer proxy, Metrics.Factory metricsFactory, @DataDirectory Path dataDirectory) {
         this.logger = logger;
         this.metricsFactory = metricsFactory;
         this.dataDirectory = dataDirectory;
         this.proxy = proxy;
         Loader.MainLoader(this.dataDirectory, this.logger);
-    }
-
-    public static UpdateRecord getUpdateRecord() {
-        return updateRecord;
     }
 
     @Subscribe
@@ -64,31 +59,28 @@ public class MavenLoader {
         if (Objects.equals(Config.getUpdaterSource(), "github")) {
             updater = new GithubUpdate(BuildConstants.VERSION);
         }
-        new Thread(() -> {
-            if (updater == null) return;
-            try {
-                UpdateRecord result = updater.getUpdate();
-                if (result != null) {
-                    if (result.hasUpdate()) {
-                        updateRecord = result;
-                        ConsoleCommandSource source = proxy.getConsoleCommandSource();
-                        source.sendMessage(Component.text("We found a MavenLoaderAPI update!", NamedTextColor.BLUE));
-                        source.sendMessage(Component.text("This machine Mavenloader version ", NamedTextColor.YELLOW)
-                                .append(Component.text(BuildConstants.VERSION, NamedTextColor.AQUA))
-                                .append(Component.text(", available updated version ", NamedTextColor.YELLOW))
-                                .append(Component.text(result.newVersion(), NamedTextColor.AQUA))
-                        );
-                        source.sendMessage(Component.text("Update information: ", NamedTextColor.YELLOW));
-                        source.sendMessage(Component.text(result.updateInfo()));
-                        source.sendMessage(Component.text("Download and update here: ", NamedTextColor.YELLOW)
-                                .append(Component.text(result.updateUrl(), NamedTextColor.AQUA)));
-                    } else {
-                        proxy.getConsoleCommandSource().sendMessage(Component.text("MavenloaderAPI is already the latest version!", NamedTextColor.GREEN));
-                    }
+        if (updater == null) return;
+        try {
+            UpdateRecord result = updater.getUpdate();
+            if (result != null) {
+                if (result.hasUpdate()) {
+                    ConsoleCommandSource source = proxy.getConsoleCommandSource();
+                    source.sendMessage(Component.text("We found a MavenLoaderAPI update!", NamedTextColor.BLUE));
+                    source.sendMessage(Component.text("This machine Mavenloader version ", NamedTextColor.YELLOW)
+                            .append(Component.text(BuildConstants.VERSION, NamedTextColor.AQUA))
+                            .append(Component.text(", available updated version ", NamedTextColor.YELLOW))
+                            .append(Component.text(result.newVersion(), NamedTextColor.AQUA))
+                    );
+                    source.sendMessage(Component.text("Update information: ", NamedTextColor.YELLOW));
+                    source.sendMessage(Component.text(result.updateInfo()));
+                    source.sendMessage(Component.text("Download and update here: ", NamedTextColor.YELLOW)
+                            .append(Component.text(result.updateUrl(), NamedTextColor.AQUA)));
+                } else {
+                    proxy.getConsoleCommandSource().sendMessage(Component.text("MavenloaderAPI is already the latest version!", NamedTextColor.GREEN));
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
-        }).start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
