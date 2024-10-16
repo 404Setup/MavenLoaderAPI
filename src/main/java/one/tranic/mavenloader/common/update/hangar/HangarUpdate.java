0f34d9e162607689a87dff5b03a50c2d3f0ce020
404Setup <153366651+404Setup@users.noreply.github.com>
2024-10-16 23:18:28 +0800

    Added Hangar API placeholder

---
+++ src/main/java/one/tranic/mavenloader/common/update/hangar/HangarUpdate.java
@@ -0,0 +1,13 @@
+package one.tranic.mavenloader.common.update.hangar;
+
+import one.tranic.mavenloader.common.update.UpdateRecord;
+import one.tranic.mavenloader.common.update.Updater;
+
+import java.io.IOException;
+
+public class HangarUpdate implements Updater {
+    @Override
+    public UpdateRecord getUpdate() throws IOException {
+        return null;
+    }
+}
